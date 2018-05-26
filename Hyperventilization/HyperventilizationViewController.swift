54ab6b9f1836d8f6631eda0958e7bcc645ea2759
Elvis Nuñez <elvisnunez@me.com>
2018-05-26 17:38:02 +0200

    Remove unused

--- Hyperventilization/HyperventilizationViewController.swift
+++ Hyperventilization/HyperventilizationViewController.swift
@@ -1,15 +1,6 @@
 import UIKit
 
 class HyperventilizationViewController: UIViewController {
-    lazy var repeatCountLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .orange
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.textAlignment = .right
-        return label
-    }()
-
     lazy var imageView: UIImageView = {
         let view = UIImageView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -17,37 +8,19 @@ class HyperventilizationViewController: UIViewController {
         return view
     }()
 
-    lazy var displayLink: CADisplayLink = {
-        let link = CADisplayLink(target: self, selector: #selector(updateLabel))
-        link.preferredFramesPerSecond = 60
-        return link
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    var phases = [3, 6, 12, 24, 48, 96]
-
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         view.backgroundColor = .white
         view.addSubview(self.imageView)
-        view.addSubview(repeatCountLabel)
         NSLayoutConstraint.activate([
             imageView.widthAnchor.constraint(equalToConstant: 150),
             imageView.heightAnchor.constraint(equalToConstant: 150),
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-
-            repeatCountLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            repeatCountLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
             ])
         animate(fromValue: 0.2, duration: 2, repeatCount: 2)
         rotate()
-
-        displayLink.add(to: RunLoop.current, forMode: .defaultRunLoopMode)
     }
 
     var pulseAnimation: CABasicAnimation!
@@ -74,10 +47,6 @@ class HyperventilizationViewController: UIViewController {
         pulseAnimation.autoreverses = true
         pulseAnimation.repeatCount = .greatestFiniteMagnitude
         imageView.layer.add(pulseAnimation, forKey: nil)
-    }
-
-    @objc func updateLabel() {
-        repeatCountLabel.text = "\(pulseAnimation.repeatCount)"
     }
 }
 
